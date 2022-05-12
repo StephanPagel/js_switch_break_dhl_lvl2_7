0d7b57ba9ea32218131250ffcb7b96e77cf7286c
StephanPagel <stephan.pagel84@gmail.com>
2022-05-12 11:42:46 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let listInput = document.getElementById('mylist');
+let listOutput = document.getElementById('masse');
+
+function showtxt() {
+    switch (listInput.value) {
+        case '0':
+            listOutput.innerHTML = `<b>Brief und Postkarte</b> <br>
+            L: 10 - 23, 5 cm
+            B: 7 - 12, 5 cm
+            H: bis 1 cm`
+            break;
+        case '1':
+            listOutput.innerHTML = `<b>DHL Paket 2 kg</b> <br>
+            bis 60 x 30 x 15 cm`
+            break;
+        case '2':
+            listOutput.innerHTML = `<b>DHL Paket 5 kg</b> <br>
+        bis 120 x 60 x 60 cm`
+            break;
+        case '3':
+            listOutput.innerHTML = `<b>DHL Paket 10 kg</b> <br>
+            bis 120 x 60 x 60 cm`
+            break;
+        default: listOutput.innerHTML = `<b>Extra große Größe</b>`
+    }
+}
+
